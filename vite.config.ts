3cba0d4f95db0250c50972eb4ea24d8efc31773f
justinmoyle <195149107+justinmoyle@users.noreply.github.com>
2026-01-29 23:36:42 -0500

    fix warning for unkown npm config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
