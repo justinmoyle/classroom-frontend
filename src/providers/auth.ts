db9bc1dceb963a136d848aaaa0cd650d72ea4aa1
justinmoyle <195149107+justinmoyle@users.noreply.github.com>
2026-02-04 22:30:34 -0500

    redirect user

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -67,7 +67,7 @@ export const authProvider: AuthProvider = {
 
       return {
         success: true,
-        redirectTo: '/dashboard',
+        redirectTo: '/',
       };
     } catch (error) {
       console.error('Login exception:', error);
